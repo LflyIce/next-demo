161726168c8dcd2621c837e13cc3356048d9f2df
LY_Fly <3454391988@qq.com>
2025-12-25 17:31:41 +0800

    feat: 优化

--- src/app/home/me/page.tsx
+++ src/app/home/me/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function MePage() {
 
@@ -11,10 +12,12 @@ export default function MePage() {
   console.log('传递的参数：', name)
   console.log('传递的多个参数：', names)
   return (
-    <div>
-      <h1>Me</h1>
-      <Link href="/home/kun">Kun</Link>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        <h1>Me</h1>
+        <Link href="/home/kun">Kun</Link>
+      </div>
+    </Suspense>
   )
     
 }
