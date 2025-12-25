01962c235a23de183f0563ced89297d9f033c764
LY_Fly <3454391988@qq.com>
2025-12-25 17:34:14 +0800

    feat: 优化

--- src/app/home/me/page.tsx
+++ src/app/home/me/page.tsx
@@ -3,21 +3,25 @@ import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
-export default function MePage() {
-
-  // 获取路由传递的参数
+function MeContent() {
   const searchParams = useSearchParams()
   const name = searchParams.get("name")
   const names = searchParams.getAll("name")
   console.log('传递的参数：', name)
   console.log('传递的多个参数：', names)
+  
+  return (
+    <div>
+      <h1>Me</h1>
+      <Link href="/home/kun">Kun</Link>
+    </div>
+  )
+}
+
+export default function MePage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <div>
-        <h1>Me</h1>
-        <Link href="/home/kun">Kun</Link>
-      </div>
+      <MeContent />
     </Suspense>
   )
-    
 }
